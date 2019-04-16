c5ed77628aeb26926413839cbebcc209be804b08
netaro-N <takebbb1+github@gmail.com>
2019-04-16 05:58:07 +0000

    OAuth 1

--- app.js
+++ app.js
@@ -4,9 +4,35 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet');
+var session = require('express-session');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github2').Strategy;
+var config = require('./config');
+
+passport.use(new GitHubStrategy({
+  clientID: config.github.CLIENT_ID,
+  clientSecret: config.github.CLIENT_SECRET,
+  callbackURL: config.github.callbackURL
+},
+  function (accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var logoutRouter = require('./routes/logout');
+//var postsRouter = require('./routes/posts');
 
 var app = express();
 app.use(helmet());
@@ -21,8 +47,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: '5b04d0ad49a2c506', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/logout', logoutRouter);
+//app.use('/posts', postsRouter);
+
+// GitHub認証の実行およびコールバック処理
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  function (req, res) {
+});
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function (req, res) {
+    res.redirect('/');
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
