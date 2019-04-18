7f900083ca636ae591a5f98b782e15f2ffafc22f
netaro-N <takebbb1+github@gmail.com>
2019-04-18 02:03:29 +0000

    fix

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const contents = [];
 
 router.post('/', function(req, res, next) {
-    const contents = [];
     contents.push(req.body.content);
     console.info('投稿されました: ' + contents);
     res.redirect(303,'/');
