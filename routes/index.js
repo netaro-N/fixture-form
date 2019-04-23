72924cd650d415b8fc22dcfd9df90e093b84be9b
netaro-N <takebbb1+github@gmail.com>
2019-04-23 01:52:36 +0000

    test delete

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,28 @@ const authenticationEnsurer = require('./authentication-ensurer');
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const title = 'Fixture-Form';
-  Post.findAll({order:[['id', 'DESC']]}).then((posts) => {
+  Post.findAll({ order: [['id', 'DESC']] }).then((posts) => {
     res.render('index', {
       title: title,
-      user:req.user ,
-      posts:posts
+      user: req.user,
+      posts: posts
     });
   });
 });
 
 router.post('/posts', authenticationEnsurer, (req, res, next) => {
-
-  const userId = req.user.provider + req.user.id;
-  Post.create({
+  if (parseInt(req.query.delete) === 1) {
+    console.log(req.body.id);
+    res.redirect(303, '/');
+  } else {
+    const userId = req.user.provider + req.user.id;
+    Post.create({
       postedBy: userId,
       content: req.body.content
     }).then(() => {
-      res.redirect(303,'/');
+      res.redirect(303, '/');
     });
-
+  }
 });
 
 module.exports = router;
