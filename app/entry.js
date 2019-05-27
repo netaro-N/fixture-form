e633cf3660e01eb8a968f9597ffb150fb433d212
netaro-N <takebbb1+github@gmail.com>
2019-05-27 07:46:27 +0000

    ajax

--- app/entry.js
+++ app/entry.js
@@ -1,1 +1,18 @@
 'use strict';
+import $ from 'jquery';
+
+$('evaluation-button').each((i, e) => {
+  const button = $(e);
+  button.click(() => {
+    const postId = button.data('post-id');
+    const userId = button.data('user-id');
+    const evaluation = button.data('user-evaluation');
+    //const nextAvailability = (availability + 1) % 3;
+    $.post(`/post/${postId}/users/${userId}`,
+      { evaluation: evaluation },
+      (data) => {
+        const nextEvaluation = !data.evaluation ? 'true' : 'false';
+        button.data('user-evaluation', nextEvaluation);
+      });
+  });
+});
