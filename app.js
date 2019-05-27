3d9857720b49905893dfcff6023de6ac06c35e4f
netaro-N <takebbb1+github@gmail.com>
2019-05-27 08:03:40 +0000

    routes

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ passport.deserializeUser(function (obj, done) {
 
 var indexRouter = require('./routes/index');
 var logoutRouter = require('./routes/logout');
+// var evaluationsRouter = require('./routes/evaluations');
 // var postsRouter = require('./routes/posts');
 
 var app = express();
@@ -93,6 +94,7 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/logout', logoutRouter);
+// app.use('/post', evaluationsRouter);
 // app.use('/posts', postsRouter);
 
 // GitHub認証の実行およびコールバック処理
