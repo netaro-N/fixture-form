d79b530066d563e21cb044512935fa3bbf8b44a1
netaro-N <takebbb1+github@gmail.com>
2019-05-28 08:51:02 +0000

    test evalutation

--- routes/evaluations.js
+++ routes/evaluations.js
@@ -1,1 +1,21 @@
 'use strict';
+const express = require('express');
+const router = express.Router();
+const authenticationEnsurer = require('./authentication-ensurer');
+const Evaluation = require('../models/evaluation');
+
+router.post('/:postId/users/:userId', authenticationEnsurer, (req,res,next) => {
+  const postId = req.params.postId;
+  const userId = req.params.userId;
+  let evaluation = req.body.evaluation;
+console.log(userId+'さんの'+postId+'への評価は'+evaluation+'です');
+  Evaluation.upsert({
+    postId:postId,
+    userId:userId,
+    evaluation:evaluation
+  }).then(() => {
+    //以下、イイね総数を取得！
+    //最後に ↓
+    res.json( {status: 'OK', evaluation: evaluation} ) // 足す, countUpEv: countUpEv } )
+  })
+})
