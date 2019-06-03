ab543a001e478188669ce3be963df1afc43d8dc8
netaro-N <takebbb1+github@gmail.com>
2019-06-03 02:49:42 +0000

    test task

--- test/test.js
+++ test/test.js
@@ -7,42 +7,17 @@ const Post = require('../models/post');
 const Evaluation = require('../models/evaluation');
 const deleteScheduleAggregate = require('../routes/index').deleteScheduleAggregate;
 
-// describe('/login', () => {
-//   before(() => {
-//     passportStub.install(app);
-//     passportStub.login({ username: 'testuser'});
-//   });
+// ログアウトのテスト
+describe('/logout', () => {
+  it('ログアウト時は/にリダイレクトされる', (done) => {
+    request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302, done)
+  });
+});
 
-//   after(() => {
-//     passportStub.logout();
-//     passportStub.uninstall(app);
-//   });
-
-//   it('ログインのためのリンクが含まれる', (done) => {
-//     request(app)
-//       .get('/login')
-//       .expect('Content-Type', 'text/html; charset=utf-8')
-//       .expect(/<a href="\/auth\/github"/)
-//       .expect(200, done);
-//   });
-
-//   it('ログイン時はユーザー名が表示される', (done) => {
-//     request(app)
-//       .get('/login')
-//       .expect(/testuser/)
-//       .expect(200, done);
-//   });
-// });
-  
-// describe('/logout', () => {
-//   it('ログアウト時は/にリダイレクトされる', (done) => {
-//     request(app)
-//       .get('/logout')
-//       .expect('Location', '/')
-//       .expect(302, done)
-//   });
-// });
-
+// ログインおよび既存の投稿の確認
 describe('/', () => {
   before(() => {
     passportStub.install(app);
@@ -54,11 +29,56 @@ describe('/', () => {
     passportStub.uninstall(app);
   });
 
-  it('トップページ', (done) => {
+  it('テストユーザーのログインと、testuserによる投稿の確認', (done) => {
     User.upsert({ userId: 'test0', username: 'testuser',thumbUrl:'hoge' }).then(() => {
-      Post.upsert({ postedBy: 'test0', content: 'test content'}).then(() => {
+      Post.upsert({ id:1, postedBy: 'test0', content: 'test content'}).then(() => {
         request(app)
           .get('/')
+          .expect(/テストユーザー/)
+          .expect(/test content/)
+          .expect(200, done);
+          // .end((err, res) => {
+          //   const match = res.html.match(/<input type="hidden" name="_csrf" value="(.*?)">/);
+          //   const csrf = match[1];
+          //   request(app)
+          //     .post('/posts')
+          //     .set('cookie', res.headers['set-cookie'])
+          //     .send({ content: 'テスト1', _csrf: csrf })
+          //     .expect('Location', '/')
+          //     .expect(302)
+              // .end((err, res) => {
+              //   const createdSchedulePath = res.headers.location;
+              //   request(app)
+              //     .get(createdSchedulePath)
+              //     // TODO 作成された予定と候補が表示されていることをテストする
+              //     .expect(/テスト予定1/)
+              //     .expect(200)
+              //     .end((err, res) => { deleteScheduleAggregate(createdSchedulePath.split('/schedules/')[1], done, err); });
+              // });
+          // });
+      });
+    });
+  });
+});
+
+// 新規投稿とその削除
+describe('/posts', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 'test', provider: 'テスト', username: 'テストユーザー', photos:[{ value: 'hoge' }] });
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('testuserによる新規投稿、確認、削除', (done) => {
+    User.upsert({ userId: 'test0', username: 'testuser',thumbUrl:'hoge' }).then(() => {
+      Post.upsert({ id:1, postedBy: 'test0', content: 'test content'}).then(() => {
+        request(app)
+          .get('/')
+          .expect(/テストユーザー/)
           .expect(/test content/)
           .expect(200, done);
           // .end((err, res) => {
