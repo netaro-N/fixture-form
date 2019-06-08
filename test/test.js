a294904a1b81cf3d33e044ea67c05d6faeed3b3e
netaro-N <takebbb1+github@gmail.com>
2019-06-08 06:00:17 +0000

    START:all delete test

--- test/test.js
+++ test/test.js
@@ -93,7 +93,7 @@ describe('/posts', () => {
 });
 
 // いいね機能のテスト
-describe('/posts', () => {
+describe('/post/:postId/users/:userId', () => {
   before(() => {
     passportStub.install(app);
     passportStub.login({ id: 0, provider: 'test', username: 'testuser', photos:[{ value: 'hoge' }] });
@@ -142,6 +142,62 @@ describe('/posts', () => {
                           deletePostAggregate(id, done, err);
                         }); 
                       });
+                  });
+              });
+            });
+      });
+    });
+  });
+});
+
+// すべての削除テスト
+describe('/posts?delete=1', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 0, provider: 'test', username: 'testuser', photos:[{ value: 'hoge' }] });
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('投稿に関する全ての情報が削除できる', (done) => {
+    User.upsert({ userId: 'test0', username: 'testuser',thumbUrl:'hoge' }).then(() => {
+      Post.upsert({ id:1, postedBy: 'test0', content: 'test content'}).then(() => {
+        request(app)
+          .get('/')
+          .end((err, res) => {
+            const match = res.text.match(/<input type="hidden" name="_csrf" value="(.*?)">/);
+            const csrf = match[1];
+
+            request(app)
+              .post('/posts')
+              .set('cookie', res.headers['set-cookie'])
+              .send({ content: 'テストです', _csrf: csrf })
+              .end((err, res) => {
+                request(app)
+                  .get('/')
+                  .end((err, res) => {
+                    const matchId = res.text.match(/<p class="test0" id="(.*?)" style="white-space:pre-wrap;">テストです/);
+                    const id = matchId[1];
+                    const userId = 'test0';
+
+                    const promiseEvaluation = new Promise((resolve) => {
+                      request(app)
+                    //postで評価をtrueに
+                      .post(`/post/${id}/users/${userId}`)
+                      .send({ evaluation:true })
+                      .end((err, res) => {
+                        if (err) done(err);
+                        resolve();
+                      });
+                    });
+
+                    //削除
+                    const promiseDeleted 
+
+
                   });
               });
             });
